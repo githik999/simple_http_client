c0828e221064d3bf04ad82b415c6f7389ef22b92
chris <kakaxi77749@gmail.com>
2022-05-19 15:45:02 +0800

    test

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # simple_http_client
 a simple http protocol implement
+
+curl -o 'g.html' -w '%{time_total} %{time_starttransfer}' -A 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36' 'https://www.google.com/search?q=webkit'
--- https_client.js
+++ https_client.js
@@ -1,8 +1,19 @@
+const fs = require('node:fs')
 const https = require('node:https')
 
-const url = 'https://www.google.com/'
+const url = 'https://www.google.com/search?q=anj'
 
 console.log(performance.now())
 https.get(url, (res) => {
-    console.log(performance.now())
+    console.log(performance.now(),res.statusCode,res.statusMessage)
+    let rawData = ''
+    res.on('data',(chunk)=>{
+        rawData += chunk
+    })
+    res.on('end',()=>{
+        console.log(performance.now())
+        fs.writeFile('g.html',rawData,(err)=>{
+
+        })
+    })
 })
